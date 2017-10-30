d0fd549e086bf08b303d213c59c9f07ef3c1b387
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-10-30 15:08:45 +0100

    2017-10-30 template inheritance

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,9 +1,20 @@
+import datetime
+
 from django.shortcuts import render
 
 
 def home(request):
     user_name = 'gulci'
+    title = 'hello'
     friends = ['karolcia', 'wiki', 'mela', 'emilka']
-    context = {"user_name": user_name, "friends": friends}
+    context = {'user_name': user_name, 'title': title, 'friends': friends}
 
-    return render(request, "base.html", context)
+    return render(request, 'base.html', context)
+
+
+def page(request):
+    title = 'a page'
+    day = datetime.datetime.now
+    context = {'title': title, 'day': day}
+
+    return render(request, 'page.html', context)
--- trydjango111/urls.py
+++ trydjango111/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-from restaurants.views import home
+from restaurants.views import home, page
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', home)
+    url(r'^$', home),
+    url(r'^page/$', page)
 ]
