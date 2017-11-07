20724899ff92033826d7c46564eb61f38cee9dc1
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2017-11-07 11:39:49 +0100

    2017-11-07 views cleaned

--- trydjango111/urls.py
+++ trydjango111/urls.py
@@ -2,14 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.views.generic import TemplateView
 
-from restaurants.views import HomeView, AboutView, ContactView
-
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', HomeView.as_view()),
-    url(r'^contact/$', ContactView.as_view()),
-    url(r'^about/$', AboutView.as_view()),
-    # if we don't add anything to the context,
-    # we don't have define the view explicite in views.py
-    url(r'^test/$', TemplateView.as_view(template_name='test.html'))
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
+    url(r'^contact/$', TemplateView.as_view(template_name='contact.html')),
+    url(r'^about/$', TemplateView.as_view(template_name='about.html')),
 ]
